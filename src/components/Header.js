a945cc5481f78c122d19f28bb99e62e8a2758031
Doğukan Özcan <dustalow58@gmail.com>
2023-10-31 17:35:51 +0300

    Testing Vercel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-
+import "./header.css" 
 export default function Header() {
   return (
     <div>
-      <input title="bi şey yaz"></input>
+      <h2 className="test">TEST WEBSİTE</h2>
     </div>
   );
 }
